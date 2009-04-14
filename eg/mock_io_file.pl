9f0cd8e301bda793ab54620a21616e6f57b514d2
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2009-04-14 20:16:10 +0000

    Import of DEXTER/Test-Builder-Mock-Class-0.01 from CPAN.
    gitpan-cpan-distribution: Test-Builder-Mock-Class
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DEXTER/Test-Builder-Mock-Class-0.01.tar.gz
    gitpan-cpan-author:       DEXTER
    gitpan-cpan-maturity:     released

---
+++ eg/mock_io_file.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use lib 'lib', '../lib';
+
+use strict;
+use warnings;
+
+use Test::More tests => 10;
+
+use constant::boolean;
+use Test::Builder::Mock::Class ':all';
+
+mock_class 'IO::File' => 'IO::File::Mock';
+
+my $io = IO::File::Mock->new;
+$io->mock_return( open => TRUE, args => [qr//, 'r'] );
+$io->mock_return( open => undef, args => [qr//, 'w'] );
+$io->mock_return_at( 0, getline => 'root:x:0:0:root:/root:/bin/bash' );
+$io->mock_expect_never( 'close' );
+
+# ok
+ok( $io->open('/etc/passwd', 'r'), '$io->open' );
+
+# first line
+like( $io->getline, qr/^root:[^:]*:0:0:/, '$io->getline' );
+
+# eof
+is( $io->getline, undef, '$io->getline' );
+
+# access denied
+ok( ! $io->open('/etc/passwd', 'w'), '$io->open' );
+
+# close was not called
+$io->mock_tally;
---
+++ lib/Test/Builder/Mock/Class/Role/Object.pm
@@ -0,0 +1,151 @@
+#!/usr/bin/perl -c
+
+package Test::Builder::Mock::Class::Role::Object;
+
+=head1 NAME
+
+Test::Builder::Mock::Class::Role::Object - Role for base object of mock class
+
+=head1 DESCRIPTION
+
+This role provides an API for defining and changing behavior of mock class.
+
+=cut
+
+use 5.006;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use Moose::Role;
+
+with 'Test::Mock::Class::Role::Object';
+
+
+use Test::Builder;
+
+
+use Exception::Base (
+    '+ignore_package' => [__PACKAGE__],
+);
+
+
+=head1 ATTRIBUTES
+
+=over
+
+=item _mock_test_builder : Test::Builder
+
+The L<Test::Builder> singleton object.
+
+=back
+
+=cut
+
+has '_mock_test_builder' => (
+    is      => 'rw',
+    default => sub { Test::Builder->new },
+);
+
+
+use namespace::clean -except => 'meta';
+
+
+## no critic RequireCheckingReturnValueOfEval
+
+=head1 METHODS
+
+=over
+
+=item <<around>> mock_tally(I<>) : Self
+
+Check the expectations at the end.  See L<Test::Mock::Class::Role::Object> for
+more description.
+
+The test passes if original C<mock_tally> method doesn't throw an exception.
+
+=cut
+
+around 'mock_tally' => sub {
+    my ($next, $self) = @_;
+
+    my $return = eval {
+        $self->$next();
+    };
+    $self->_mock_test_builder->is_eq($@, '', 'mock_tally()');
+
+    return $return;
+};
+
+
+=item <<around>> mock_invoke( I<method> : Str, I<args> : Array ) : Any
+
+Returns the expected value for the method name and checks expectations.  See
+L<Test::Mock::Class::Role::Object> for more description.
+
+The test passes if original C<mock_tally> method doesn't throw an exception.
+
+=cut
+
+around 'mock_invoke' => sub {
+    my ($next, $self, $method, @args) = @_;
+
+    my ($return, @return);
+    eval {
+        if (wantarray) {
+            @return = $self->$next($method, @args);
+        }
+        else {
+            $return = $self->$next($method, @args);
+        };
+    };
+    $self->_mock_test_builder->is_eq($@, '', "mock_invoke($method)");
+
+    return wantarray ? @return : $return;
+};
+
+
+1;
+
+
+=back
+
+=begin umlwiki
+
+= Class Diagram =
+
+[                                <<role>>
+                    Test::Builder::Mock::Class::Role::Object
+ -----------------------------------------------------------------------------
+ #_mock_test_builder : Test::Builder
+ -----------------------------------------------------------------------------
+ +<<around>> mock_tally() : Self
+ +<<around>> mock_invoke( method : Str, args : Array ) : Any
+                                                                              ]
+
+=end umlwiki
+
+=head1 SEE ALSO
+
+L<Test::Mock::Class>.
+
+=head1 BUGS
+
+The API is not stable yet and can be changed in future.
+
+=head1 AUTHOR
+
+Piotr Roszatycki <dexter@cpan.org>
+
+=head1 LICENSE
+
+Based on SimpleTest, an open source unit test framework for the PHP
+programming language, created by Marcus Baker, Jason Sweat, Travis Swicegood,
+Perrick Penet and Edward Z. Yang.
+
+Copyright (c) 2009 Piotr Roszatycki <dexter@cpan.org>.
+
+This program is free software; you can redistribute it and/or modify it
+under GNU Lesser General Public License.
